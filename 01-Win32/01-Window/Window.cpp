61073c0410a007d6269cdf7e31307fba184befd9
mohit264 <mohit.dharma@gmail.com>
2021-06-29 23:41:23 +0530

    Describe the code of the Window

--- 01-Win32/01-Window/Window.cpp
+++ 01-Win32/01-Window/Window.cpp
@@ -1,11 +1,44 @@
+// Why header files?
+// During programming we call APIs provided by SDK. So for compiler to convert from high level to machine level
+// which are not defined by you but consume by you to tell compiler that such kind of function will be called
+// in the code and it will use APIs compiled code during compilation process.
+// coco.h - in Mac
 #include <windows.h>
 
 // Global Functions Declarations
+
+// Declaring the function / prototype to let compiler know that body of this function will come later
+// When there are several function it is good practice to create separate header file for them.
+
+// This is callback function in event-driven architecture
+// This should be in special addresss (Far segment) CALLBACK is typedef of _far_pascal
+// WndProc name is by convention not the compulsion
+// std calling convention or pascal calling convention which pass the parameters from right to left
+
+// Parameters
+// 1. HWND is internally unique unsigned 32 int
+// Handle to Window
+// What is handle? 
+// Handles are keys in the OS tables which represents which resources are currently active in memory.
+// So if  there are 50 windows open in desktop there are 50 handles(keys) present in Windows assuming
+// 50 different application
+
+// 2. UINT -- Event converted into unique message id 
+// Why  event is sent as in integer because event is coming from hardware device which is identified as integer.
+
+// 3,4. W(ord)PARAM and L(ong)PARAM (Message Parameters)
+// Description about the event, who is event sender means every message has it's own attributes.
+// if the less attributed message i.e. (16-bit) and if there more attributes to the message.
+// then it can be passed from LPARAM.
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 // Global Variable Declarations
 
 // Entry point Functions
+// WINMAIN name is must
+// It is internally WINMAINCRTSTARTUP
+// This function is also pre-decided by OS
+// Therefore OS decided parameters and OS decided return value.
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
 {
     // Local Variable Declarations
@@ -77,4 +110,3 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
     }
     return (DefWindowProc(hwnd, iMsg, wParam, lParam));
 }
-         
