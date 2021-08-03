d90bfb351c7a32578293edaf0ad491db13db17d0
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2021-08-03 22:41:05 +0530

    Include MyMath.Lib in link command

---
+++ 01-Win32/10-Dll/01-Linking/01-Implicit/01-LibOnCMD/MyMath.h
@@ -0,0 +1,6 @@
+//By specifying this even if this header files get included multiple 
+// with help of below pragma, it includes the header file only once
+#pragma once
+extern "C" int MakeSquare(int);
+
+
