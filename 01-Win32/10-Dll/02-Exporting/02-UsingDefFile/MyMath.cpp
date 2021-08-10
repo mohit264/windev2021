c9f996ec1419c06699be2921e89a1d63204c785a
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2021-08-10 22:05:01 +0530

    link MyMath.obj /DLL /DEF:MyMath.def user32.lib gdi32.lib /SUBSYSTEM:WINDOWS

---
+++ 01-Win32/10-Dll/02-Exporting/02-UsingDefFile/MyMath.cpp
@@ -0,0 +1,38 @@
+#pragma once
+#include<Windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hDll, DWORD dwReason, LPVOID lpReserved)
+{
+    // code
+    switch (dwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+        /* code */
+        break;
+        case DLL_THREAD_ATTACH:
+        /* code */
+        break;
+        case DLL_PROCESS_DETACH:
+        /* code */
+        break;
+        case DLL_THREAD_DETACH:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+    return TRUE;
+}
+
+// Extern C is required for Explicit linking
+// Removed __declspec(dllexport) as it is not needed and taken care by DEF file
+// Linker read all the functions in DEF and create .exp file for Export Function table
+// to be added in DLL's export function
+extern "C" int MakeCube(int num)
+{
+    //code
+    int cube = num * num * num;
+
+    return cube;
+}
