c8b8891c5fd44fa57586bcd44325e2edfd9495e6
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2021-08-10 22:05:55 +0530

    link command added

--- 01-Win32/10-Dll/02-Exporting/02-UsingDefFile/MyMath.cpp
+++ 01-Win32/10-Dll/02-Exporting/02-UsingDefFile/MyMath.cpp
@@ -29,6 +29,8 @@ BOOL WINAPI DllMain(HINSTANCE hDll, DWORD dwReason, LPVOID lpReserved)
 // Removed __declspec(dllexport) as it is not needed and taken care by DEF file
 // Linker read all the functions in DEF and create .exp file for Export Function table
 // to be added in DLL's export function
+
+//link MyMath.obj /DLL /DEF:MyMath.def user32.lib gdi32.lib /SUBSYSTEM:WINDOWS
 extern "C" int MakeCube(int num)
 {
     //code
